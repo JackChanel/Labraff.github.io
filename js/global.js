205390a46c1c3344fc8f6f9774372fa5303ed935
labraff <1667716062@qq.com>
2021-01-04 15:20:33 +0800

    version 1.3.3 js源转国内镜像

--- js/global.js
+++ js/global.js
@@ -1,6 +1,7 @@
 
+import '../node_modules/vue/dist/vue.js'
+import '../node_modules/less/dist/less.js'
+
 import 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js'
 import 'https://cdnjs.cloudflare.com/ajax/libs/less.js/3.11.1/less.min.js'
 
-import '../node_modules/vue/dist/vue.js'
-import '../node_modules/less/dist/less.js'
