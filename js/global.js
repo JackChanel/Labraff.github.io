921c9721ad6f7a203532288aaf6d49a7c1c775d9
labraff <1667716062@qq.com>
2021-01-04 15:52:51 +0800

    version 1.3.4 登录页背景图修复

--- js/global.js
+++ js/global.js
@@ -1,6 +1,6 @@
 
-import '../node_modules/vue/dist/vue.js'
-import '../node_modules/less/dist/less.js'
+// import '../node_modules/vue/dist/vue.js'
+// import '../node_modules/less/dist/less.js'
 
 import 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js'
 import 'https://cdnjs.cloudflare.com/ajax/libs/less.js/3.11.1/less.min.js'
